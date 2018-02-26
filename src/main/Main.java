fcad52d8099eee3f4f379b1d6880cedb80a17125
DaStinkingScaliwag <gcperk20@gmail.com>
2018-02-25 20:47:07 -0500

    Solve questions 6 and 7

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 public class Main {
 	static Scanner in = new Scanner(System.in);
 	public static void main(String[] args) {
-		problemFive();
+		problemSeven();
 	}
 	
 	public static String format(double n, String pattern) {
@@ -94,10 +94,69 @@ public class Main {
 			k = in.nextInt();
 			f(start,1);
 		}
-
 	}
-    
-	public static void f(int n, int i) {
+	
+	public static void problemSix() {
+		start = in.nextInt();
+		int n = in.nextInt();
+		System.out.println(format(chord2(start/(int)(Math.pow(2, n))), "#.#"));
+	}
+	
+	public static void problemSeven() {
+		int[] reg = new int[4];
+		reg[0] = 0;
+		int iar = in.nextInt();
+		for (int i =1; i < 4; i++) reg[i] = in.nextInt();
+		int[] op = new int[3];
+		int opCode = in.nextInt();
+		for (int i = 0; i < 3; i++) op[i] = in.nextInt();
+		switch (opCode){
+		case 0:
+			if (op[2] != 0) {
+				reg[op[2]] = op[0] + reg[op[1]];
+			}
+			iar++;
+			break;
+		case 1:
+			if (op[2] != 0) {
+				reg[op[2]] = reg[op[0]] + reg[op[1]];
+			}
+			iar++;
+			break;
+		case 2:
+			if (reg[op[1]] != reg[op[2]]) {
+				iar = op[0];
+			} else {
+				iar++;
+			}
+		case 3:
+			if (reg[op[1]] == reg[op[2]]) {
+				iar = op[0];
+			} else {
+				iar++;
+			}
+		}
+		System.out.println(iar+" 0 "+reg[1]+" "+reg[2]+" "+reg[3]);
+	}
+	
+	public static double chord1(double x) {
+		double r = 3438;
+		System.out.println("chord 1 x:"+x+" chord:"+(int)Math.sqrt(Math.pow(2*r,2)-Math.pow(chord2(x), 2)));
+		return Math.sqrt(Math.pow(2*r,2)-Math.pow(chord2(x), 2));
+	}
+	
+	public static double chord2(double x) {
+		double r = 3438;
+		if (x == 60) {
+			return r;
+		} else if (x == 90) {
+			return 4862;
+		}
+		System.out.println("chord 2 x:"+x+" chord:"+(int)Math.sqrt(r*(2*r - chord1(2*x))));
+		return Math.sqrt(r*(2*r - chord1(2*x)));
+	}
+	
+    public static void f(int n, int i) {
 		int product = 1;
 		int copyN = n;
 		while (copyN > 0) {
